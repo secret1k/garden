d413bae9f4b8626d58325102b03c25549dd1d6fd
secret1k <none>
2024-12-09 01:34:13 +0300

    db context

---
+++ garden/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+public class ApplicationContext : DbContext
+{
+    public DbSet<Product> Products { get; set; } = null!;
+    public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+    {
+        Database.EnsureCreated();   // создаем базу данных при первом обращении
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>().HasData(
+            new Product { ProductId = 1, Name = "prod1", CategoryId = 1, Description = "good", Price = 100, Img = "none" },
+            new Product { ProductId = 2, Name = "prod2", CategoryId = 1, Description = "mid", Price = 99, Img = "none" },
+            new Product { ProductId = 3, Name = "prod3", CategoryId = 2, Description = "good", Price = 130, Img = "none" }
+            );
+    }
+}
---
+++ garden/Models/product.cs
@@ -0,0 +1,9 @@
+﻿public class Product
+{
+    public int ProductId { get; set; }
+    public string Name { get; set; } = "";
+    public int CategoryId { get; set; }
+    public string Description { get; set; } = "";
+    public int Price { get; set; }
+    public string Img { get; set; } = "";
+}
