ad3e91df6a808111489c1719fecd8d68b0743892
secret1k <none>
2024-12-08 21:13:09 +0300

    test3

---
+++ garden/Class.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+public class ApplicationContext : DbContext
+{
+    public DbSet<User> Users { get; set; } = null!;
+    public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+    {
+        Database.EnsureCreated();   // создаем базу данных при первом обращении
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().HasData(
+            new User { Id = 1, Name = "Tom", Age = 37 },
+            new User { Id = 2, Name = "Bob", Age = 41 },
+            new User { Id = 3, Name = "Sam", Age = 24 }
+            );
+    }
+}
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public int Age { get; set; }
+}
