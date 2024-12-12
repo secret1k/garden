ea7a4d5f321f40cc379bc9aa476c90824fa8d83a
secret1k <none>
2024-12-13 00:43:50 +0300

    ((

--- garden/ApplicationContext.cs
+++ garden/ApplicationContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 public class ApplicationContext : DbContext
 {
+    public DbSet<Category> Categories { get; set; } = null!;
     public DbSet<Product> Products { get; set; } = null!;
     public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
     {
@@ -8,6 +9,10 @@ public class ApplicationContext : DbContext
     }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Category>().HasData(
+            new Category { CategoryId = 1, Name = "cat1", Img = "none" },
+            new Category { CategoryId = 2, Name = "cat2", Img = "none" }
+            );
         modelBuilder.Entity<Product>().HasData(
             new Product { ProductId = 1, Name = "prod1", CategoryId = 1, Description = "good", Price = 100, Img = "none" },
             new Product { ProductId = 2, Name = "prod2", CategoryId = 1, Description = "mid", Price = 99, Img = "none" },
